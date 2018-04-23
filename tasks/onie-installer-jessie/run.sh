ff72bb44009bd2def5f4df78bc47d1566e4095e8
Tyler Heucke <tyler.heucke@dell.com>
2018-04-23 15:09:41 -0700

    Fix opx-onie-installer CI script location
    Change-Id: Iea4354c9402d65cb9775ecd091e3df26cf58a21e
    Signed-off-by: Tyler Heucke <tyler.heucke@dell.com>

--- tasks/onie-installer-jessie/run.sh
+++ tasks/onie-installer-jessie/run.sh
@@ -8,7 +8,7 @@ set -x
 
 rsync -a opx-onie-installer/ /mnt/opx-onie-installer
 pushd /mnt
-  /opt/opx-build/scripts/opx_run opx_rel_pkgasm.py \
+  /opt/opx-build/scripts/opx_rel_pkgasm.py \
     -b opx-onie-installer/release_bp/OPX_dell_base.xml \
     --dist "$DIST" -n 0
 popd
