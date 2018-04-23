579aa35e7de58977f28fa015bbdbe84efc030c4d
Tyler Heucke <tyler.heucke@dell.com>
2018-04-23 15:05:46 -0700

    Initial commit of opx-onie-installer pipeline
    Change-Id: I588654aa1c6275bea37fe9c8613ec61a60e31b83
    Signed-off-by: Tyler Heucke <tyler.heucke@dell.com>

---
+++ tasks/onie-installer-jessie/run.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+set -o errexit
+set -o nounset
+set -o pipefail
+
+set -x
+
+rsync -a opx-onie-installer/ /mnt/opx-onie-installer
+pushd /mnt
+  /opt/opx-build/scripts/opx_run opx_rel_pkgasm.py \
+    -b opx-onie-installer/release_bp/OPX_dell_base.xml \
+    --dist "$DIST" -n 0
+popd
+
+find /mnt -name 'PKGS*' -exec mv -t opx-onie-installer-artifacts/ {} +
