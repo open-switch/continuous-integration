3bae7cca42280f61e5ac7a6db3b08d9476cb3edc
Mithil Patel <Mithil_patel@dell.com>
2019-02-13 15:03:39 -0800

    Moving aptly variable to global

--- buildkite/aptly-bootstrap.sh
+++ buildkite/aptly-bootstrap.sh
@@ -1,6 +1,7 @@
 (
 # Common configuration ########################################################
-
+set -x
+export APTLY_KEY=AD5073F1        # This is a temporary solution to fix GPG publish errors
 yum -y install python36
 easy_install-3.6 pip
 /usr/local/bin/pip3 install --upgrade pip
@@ -34,7 +35,6 @@ PRE
 # Agent-specific configuration ################################################
 
 cat <<'PRE' | tee -a /etc/buildkite-agent/hooks/pre-command
-export APTLY_KEY=AD5073F1        # This is a temporary solution to fix GPG publish errors
 echo "$APTLY_KEY" | gpg --import || true
 echo "$APTLY_KEY" | gpg --no-default-keyring --keyring trustedkeys.gpg --import || true
 aws s3 sync --delete s3://aptly.openswitch.net/ /aptly
